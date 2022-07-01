016d5d1ed8f8fdb913b48a9030c026c7647ad68f
handysome6 <handysome6@gmail.com>
2022-07-01 17:12:34 +0800

    added calibrate camera widgets

--- qt/qt_calibrate.py
+++ qt/qt_calibrate.py
@@ -1,8 +1,87 @@
+from hashlib import algorithms_available
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QGroupBox, QGridLayout, QVBoxLayout
+from PySide6.QtWidgets import QLabel, QPushButton, QLineEdit, QCheckBox
 from PySide6 import QtCore, QtWidgets, QtGui
 
 class CalibWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__()
         self.setWindowTitle("Calibrate camera")
+        self._calib_single_camera_group_box()
+        self._calib_dual_camera_group_box()
+        self._start_calib_config_group_box()
+
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.addWidget(self.single_group_box)
+        self.main_layout.addWidget(self.dual_group_box)
+        self.main_layout.addWidget(self.config_group_box)
+
+
+    def _calib_single_camera_group_box(self):
+        self.single_group_box = QGroupBox("Single Camera Calibration")
+        # indenp checker box
+        independent_single_label = QLabel("Calibrate single cameras independently:")
+        independent_single_checker = QCheckBox()
+        # left 
+        single_left_label = QLabel("Calib left camera path:")
+        single_left_path = QLineEdit("Please select folder...")
+        single_left_btn = QPushButton("...")
+        # right 
+        single_right_label = QLabel("Calib right camera path:")
+        single_right_path = QLineEdit("Please select folder...")
+        single_right_btn = QPushButton("...")
+
+        layout = QGridLayout()
+        layout.addWidget(independent_single_label,   0,0,1,2)
+        layout.addWidget(independent_single_checker, 0,2, alignment=Qt.AlignCenter)
+        layout.addWidget(single_left_label,  1,0)
+        layout.addWidget(single_left_path,   1,1)
+        layout.addWidget(single_left_btn,    1,2)
+        layout.addWidget(single_right_label, 2,0)
+        layout.addWidget(single_right_path,  2,1)
+        layout.addWidget(single_right_btn,   2,2)
+
+        self.single_group_box.setLayout(layout)
+
+    def _calib_dual_camera_group_box(self):
+        self.dual_group_box = QGroupBox("Dual Camera Calibration")
+        # dual
+        dual_label = QLabel("Calib dual camera path:")
+        dual_path = QLineEdit("Please select folder...")
+        dual_btn = QPushButton("...")
+        # checker: Fix intrinsic
+        fix_intrinsic_label = QLabel("Fix intrinsic params when dual calibration:")
+        fix_intrinsic_checker = QCheckBox()
+
+        layout = QGridLayout()
+        layout.addWidget(dual_label,  0,0)
+        layout.addWidget(dual_path,   0,1)
+        layout.addWidget(dual_btn,    0,2)
+        layout.addWidget(fix_intrinsic_label, 1,0,1,2)
+        layout.addWidget(fix_intrinsic_checker, 1,2, alignment=Qt.AlignCenter)
+
+        self.dual_group_box.setLayout(layout)
+
+    def _start_calib_config_group_box(self):
+        self.config_group_box = QGroupBox("Calibration Configuration")
+        # camera model save as file
+        model_save_label = QLabel("Camera model save as: ")
+        model_save_path = QLineEdit("Please input target...")
+        model_save_btn = QPushButton("...")
+        # fisheye 
+        fisheye_label = QLabel("FishEye Mode: ")
+        fisheye_checker = QCheckBox()
+        # start button
+        start_calib_btn = QPushButton("Start Calibration")
+
+        layout = QGridLayout()
+        layout.addWidget(model_save_label, 0,0)
+        layout.addWidget(model_save_path, 0,1)
+        layout.addWidget(model_save_btn, 0,2)
+        layout.addWidget(fisheye_label, 1,0)
+        layout.addWidget(fisheye_checker, 1,2, alignment=Qt.AlignCenter)
+        layout.addWidget(start_calib_btn, 2,0,1,3)
+
+        self.config_group_box.setLayout(layout)
+        
--- qt/qt_main.py
+++ qt/qt_main.py
@@ -1,7 +1,8 @@
 import sys
 from pathlib import Path
+from time import sleep
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QSize, QThread
 from PySide6.QtWidgets import QPushButton, QComboBox, QLabel, QGridLayout, QHBoxLayout
 from PySide6.QtWidgets import QScrollArea, QWidget, QTextEdit
 from PySide6.QtWidgets import QListWidget, QListWidgetItem, QListView
@@ -9,12 +10,16 @@ from PySide6.QtGui import QPalette, QIcon
 from qt.qt_calibrate import CalibWindow
 from qt.qt_take_photo import TakePhotoWindow
 from qt.qt_view_measure import ViewMeasureWindow
+from concurrent.futures import ThreadPoolExecutor
+import subprocess
+
 
 class MyWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__()
         self.setWindowTitle('Ensightful Project')
 
+        # components
         self.btn_calibrate_camera = QPushButton("Calibrate Camera...")
         self.model_selection = QComboBox()
         self.label_recent = QLabel("Recent Images:")
@@ -24,20 +29,25 @@ class MyWidget(QWidget):
         self.copy_right = QLabel("© Ensightful Ltd. All Rights Reserved.")
         self.copy_right.setStyleSheet("color: gray;")
         
-
+        # grid layout 
         self._init_model_menu()
         self.grid_layout = QGridLayout(self)
         self.grid_layout.addWidget(self.btn_calibrate_camera, 0, 0)
         self.grid_layout.addWidget(self.model_selection, 0, 1)
         self.grid_layout.addWidget(self.label_recent, 1, 0)
-        self._init_dispaly_image()
+        self.img_list_region = QListWidget()
+        self.grid_layout.addWidget(self.img_list_region, 2, 0, 1, 2)
         self.grid_layout.addWidget(self.btn_take_photo, 3, 0)
         self.grid_layout.addWidget(self.btn_proceed_measure, 3, 1)
         self.grid_layout.addWidget(self.copy_right, 4,0, 1, 2, alignment=Qt.AlignCenter)
 
+        # setup btn 
         self.btn_calibrate_camera.clicked.connect(self._open_calib_window)
         self.btn_take_photo.clicked.connect(self._open_take_photo_window)
         self.btn_proceed_measure.clicked.connect(self._open_view_measure_window)
+
+        # load images icons
+        self._init_dispaly_image()
 
     def _init_model_menu(self):
         # search and addItem
@@ -49,25 +59,32 @@ class MyWidget(QWidget):
         PIL_image = Image.fromarray(numpy_image.astype('uint8'), 'RGB')
         icon = QPixmap.fromImage(ImageQt(PIL_image))
 
-        self.model_selection.addItem(icon, "example model.npz")
-        self.model_selection.addItem(icon, "example model2.npz")
+        model_path = Path('datasets/calibration_data')
+        models = model_path.glob('*.npz')
+        for model in models:
+            self.model_selection.addItem(icon, model.name)
 
     def _init_dispaly_image(self):
-        # init dispaly region
-        list_widget = QListWidget()
-        list_widget.setViewMode(QListWidget.IconMode)
-        list_widget.setResizeMode(QListWidget.Adjust)
-        list_widget.setMovement(QListView.Static)
+        self.img_list_region.setViewMode(QListWidget.IconMode)
+        self.img_list_region.setResizeMode(QListWidget.Adjust)
+        self.img_list_region.setMovement(QListView.Static)
+        self.img_list_region.setIconSize(QSize(230,230))
 
-        # init display images
-        test_folder = Path('icons')
-        imgs = test_folder.glob("*.jpg")
-        for img in imgs:
-            path = str(img)
-            item = QListWidgetItem(QIcon(path), path)
-            list_widget.addItem(item)
+        def load_image(img_path):
+            path = str(img_path)
+            return QIcon(path), img_path.name
 
-        self.grid_layout.addWidget(list_widget, 2, 0, 1, 2)
+        executor = ThreadPoolExecutor(5)
+        test_folder = Path('datasets/0617_IMX477_5000/test')
+        imgs = list(test_folder.glob("*.jpg"))
+        for img_path in imgs[:5]:
+            future = executor.submit(load_image, (img_path))
+            future.add_done_callback(self.insert_images)
+            
+    def insert_images(self, future):
+        icon, name = future.result()
+        item = QListWidgetItem(icon, name)
+        self.img_list_region.addItem(item)
 
     @QtCore.Slot()
     def _open_calib_window(self):
@@ -76,8 +93,11 @@ class MyWidget(QWidget):
 
     @QtCore.Slot()
     def _open_take_photo_window(self):
-        self.take_photo_window = TakePhotoWindow(self)
-        self.take_photo_window.show()
+        # self.take_photo_window = TakePhotoWindow(self)
+        # self.take_photo_window.show()
+        command = "raspistill -3d sbs -t 0 -fp -h 3040 -w 8112 -dt -o datasets/test/sbs_%d.jpg -k"
+        p = subprocess.Popen("ls -lh", stdout=subprocess.PIPE, shell=True)
+        print(p.communicate())
 
     @QtCore.Slot()
     def _open_view_measure_window(self):
