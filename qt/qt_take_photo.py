39c2a7ebf523e7aebdfd3e21430ed1f3d0cf74a6
handysome6 <handysome6@gmail.com>
2022-08-09 15:14:58 +0800

    updated qt_take_photo GUI

--- qt/qt_take_photo.py
+++ qt/qt_take_photo.py
@@ -1,7 +1,8 @@
 import sys
 
 from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QPushButton, QSizePolicy
-from PyQt5.QtGui import QWindow
+from PyQt5.QtGui import QWindow, QIcon
+from PyQt5.QtCore import QSize
 
 from gstreamer.gst_camera import CameraWithPreview, CameraNoPreview
 
@@ -13,19 +14,29 @@ class TakePhotoWindow(QWidget):
         # init camera
         self.camera_0 = CameraWithPreview(sensor_id=0)
         self.camera_1 = CameraNoPreview(sensor_id=1)
-        # fetch embedded window handle
+
+        # preview widget
+        # fetch embedded window handle, embed x11 window to qt widget
         preview_window_xid = self.camera_0.get_window_xid()
-        # embed x11 window to qt widget
         embbed_preview_window = QWindow.fromWinId(preview_window_xid)
         embbed_preview_widget = QWidget.createWindowContainer(embbed_preview_window)
+        # embbed_preview_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        # camera button
+        camera_button = QPushButton()
+        icon = QIcon("qt/camera_icon.png")
+        camera_button.setIcon(icon)
+        camera_button.setIconSize(QSize(85, 85))
+        camera_button.setFlat(True)
+        sp = QSizePolicy()
+        sp.setHorizontalStretch(True)
+        camera_button.setSizePolicy(sp)
+        camera_button.clicked.connect(self._slot_capture)
 
         # organize layout
         layout = QHBoxLayout(self)
         layout.addWidget(embbed_preview_widget)
-        capture_button = QPushButton("capture")
-        layout.addWidget(capture_button)
-        #capture_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        capture_button.clicked.connect(self._slot_capture)
+        layout.addWidget(camera_button)
     
     def _slot_capture(self):
         """slot to take snapshot for both cameras"""
