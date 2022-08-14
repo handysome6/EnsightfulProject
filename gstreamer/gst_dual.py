0be5a27be150ae07ddabcec75970e55bb0822448
handysome6 <handysome6@gmail.com>
2022-08-14 12:54:10 +0800

    added dual camera mix

--- gstreamer/gst_dual.py
+++ gstreamer/gst_dual.py
@@ -61,17 +61,26 @@ class CameraWithPreview(Gtk.Window):
 
     def _init_pipeline_gtksink(self, sensor_id):
         Gst.init(None)
-        gtksink_pipeline = f"nvarguscamerasrc sensor-id={sensor_id} ! \
-video/x-raw(memory:NVMM), width={RESOLUTION[0]}, height={RESOLUTION[1]}, format=NV12, framerate=30/1 ! \
+        dual_pipeline = f"\
+nvarguscamerasrc sensor-id={sensor_id} ! video/x-raw(memory:NVMM), width={RESOLUTION[0]}, height={RESOLUTION[1]}, format=NV12, framerate=30/1 ! \
 tee name=t \
-t. ! queue ! valve drop=1 name=capture_valve ! \
-    nvvidconv ! video/x-raw, format=RGBA ! \
-    appsink name=capture_sink async=false emit-signals=1 \
-t. ! queue ! \
-    nvvidconv ! video/x-raw, width={RESOLUTION[0]//4}, height={RESOLUTION[1]//4}  ! \
-    gtksink name=gtksink"
+    t. ! \
+        queue ! nvvidconv ! video/x-raw, width={RESOLUTION[0]//4}, height={RESOLUTION[1]//4} ! \
+            gtksink \
+    t. ! \
+        valve drop=1 name=valve_0 ! nvvidconv ! video/x-raw, format=RGBA ! \
+            mix.sink_0 \
+\
+nvarguscamerasrc sensor-id={1-sensor_id} ! video/x-raw(memory:NVMM), width={RESOLUTION[0]}, height={RESOLUTION[1]}, format=NV12, framerate=30/1 ! \
+        valve drop=1 name=valve_1 ! nvvidconv ! video/x-raw, format=RGBA ! \
+            mix.sink_1 \
+\
+compositor name=mix  sink_0::xpos=0 sink_0::ypos=0  sink_1::xpos=4032 sink_1::ypos=0 ! \
+    video/x-raw,format=RGBA ! \
+    appsink name=capture_sink async=false emit-signals=1 "
+        
         # print(gtksink_pipeline)
-        self.pipeline = Gst.parse_launch(gtksink_pipeline)
+        self.pipeline = Gst.parse_launch(dual_pipeline)
         self.capture_valve = self.pipeline.get_by_name("capture_valve")
         self.capture_sink = self.pipeline.get_by_name("capture_sink")
         # message handler
@@ -178,112 +187,6 @@ t. ! queue ! \
         print("\nPipeline finished gracefully.")
 
 
-
-class CameraNoPreview():
-    def __init__(self, sensor_id=1):
-        # params
-        self.sensor_id = sensor_id
-        self.preview_frame = None
-        self.read_thread = None
-        self.running = False
-        self.FRAME_COUNT = 0
-        self.capture_frame = None
-
-        self._init_pipeline_fakesink(sensor_id)
-        self.start()
-
-    def _init_pipeline_fakesink(self, sensor_id):
-        Gst.init(None)
-        fakesink_pipeline = f"nvarguscamerasrc sensor-id={sensor_id} ! \
-video/x-raw(memory:NVMM), width={RESOLUTION[0]}, height={RESOLUTION[1]}, format=NV12, framerate=10/1 ! \
-    queue ! valve drop=1 name=capture_valve ! \
-    nvvidconv ! video/x-raw, format=RGBA ! \
-    appsink name=capture_sink async=false emit-signals=1"
-        # print(fakesink_pipeline)
-        self.pipeline = Gst.parse_launch(fakesink_pipeline)
-        self.capture_valve = self.pipeline.get_by_name("capture_valve")
-        self.capture_sink = self.pipeline.get_by_name("capture_sink")
-        # message handler
-        self.bus = self.pipeline.get_bus()
-        self.bus.add_signal_watch()
-        self.bus.connect('message', self._on_message)
-        print("init pipeline with fakesink finished")
-
-    def start(self):
-        if self.running:
-            print('Video capturing is already running')
-            return None
-
-        print("starting pipeline...")
-        # start playing 
-        self.pipeline.set_state(Gst.State.PLAYING)
-        self.loop = GLib.MainLoop()
-        # start running 
-        self.running = True
-        self.read_thread = threading.Thread(target=self._run_main_loop)
-        self.read_thread.start()
-
-        # add caputure sink handler
-        self.capture_sink.connect("new-sample", self._capture_on_buffer, None)
-
-
-    def capture(self):
-        """open the valve, let buffer flow over"""
-        self.capture_valve.set_property("drop", False)
-        while self.capture_frame is None:
-            time.sleep(0.1)
-        capture_frame = self.capture_frame.copy()
-        # reset capture_frame for next capture
-        self.capture_frame = None
-        return capture_frame
-        
-    def _capture_on_buffer(self, sink, data):
-        """Callback serving capture function"""
-        # Emit 'pull-sample' signal
-        sample = sink.emit("pull-sample")  # Gst.Sample
-        if isinstance(sample, Gst.Sample):
-            self.capture_frame = extract_buffer(sample)
-            print(f"\n\n>>> Captured {type(self.capture_frame)} "
-                f"shape {self.capture_frame.shape} "
-                f"type {self.capture_frame.dtype} <<<\n")
-            # Once fetched a frame, turn down the valve
-            self.capture_valve.set_property("drop", True)
-            return Gst.FlowReturn.OK
-        else:
-            return Gst.FlowReturn.ERROR
-
-
-    def _on_message(self, bus: Gst.Bus, message: Gst.Message):
-        mtype = message.type
-        if mtype == Gst.MessageType.EOS:
-            print("End of stream")
-            self.loop.quit()
-        elif mtype == Gst.MessageType.ERROR:
-            err, debug = message.parse_error()
-            print(err, debug)
-            self.loop.quit()
-        elif mtype == Gst.MessageType.WARNING:
-            err, debug = message.parse_warning()
-            print(err, debug)
-
-        return True
-
-    def _run_main_loop(self):
-        try:
-            self.loop.run()
-        except Exception:
-            traceback.print_exc()
-            self.loop.quit()
-
-    def stop(self):
-        self.running = False
-        self.pipeline.set_state(Gst.State.NULL)
-        self.loop.quit()
-        self.preview_frame = None
-        # Kill the thread
-        self.read_thread.join()
-        self.read_thread = None
-        print("\nPipeline finished gracefully.")
 
 
 
