0ffdd719903f69908c2325a6e18cf22f29c3386e
Albert XU <71578621+Albert57060839@users.noreply.github.com>
2022-08-17 15:58:43 +0800

    Update ruler.py
    show dx, dy, dz and points coordintes

--- measure/ruler.py
+++ measure/ruler.py
@@ -28,9 +28,13 @@ class Ruler():
 
         # click to get segment
         point1, point2 = self.endpoints[-2:]
+        # print(point1, point2)
+        # print('world')
         world_coord1 = Ruler.get_world_coord_Q(Q, point1[0], point1[1])
         world_coord2 = Ruler.get_world_coord_Q(Q, point2[0], point2[1])
-
+        # dx, dy, dz = world_coord2[0] - world_coord1[0], world_coord2[1] - world_coord1[1], world_coord2[2] - world_coord1[2]
+        # print(world_coord1, world_coord2)
+        # print(f"dx: {dx:.2f}mm, dy: {dy:.2f}mm, dz: {dz:.2f}mm,")
         self.segment_len = cv2.norm(world_coord1, world_coord2)
         return self.segment_len
 
@@ -131,15 +135,44 @@ class Ruler():
 
     def show_result(self):
         point1, point2 = self.endpoints[-2:]
+        world_coord1 = Ruler.get_world_coord_Q(self.Q, point1[0], point1[1])
+        world_coord2 = Ruler.get_world_coord_Q(self.Q, point2[0], point2[1])
         point1 = point1[0].astype(np.int32)
         point2 = point2[0].astype(np.int32)
-
+        height, width = self.left_img.shape[:2]
         line_thickness = 8
         show_img = self.left_img.copy()
+        # show segment length
         cv2.line(show_img, point1, point2, (0,255,0), thickness=line_thickness)
-        cv2.putText(show_img, f"Length of segment: {self.segment_len:.2f}mm",
-            (0,100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,255,0), 3)
-        imshow('Image Result', show_img)
+        cv2.putText(show_img, f"Length of segment: {self.segment_len:.1f}mm",
+            (20,150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,255,0), 10)
+        # show different x, y z
+        dx, dy, dz = world_coord2[0] - world_coord1[0], world_coord2[1] - world_coord1[1], world_coord2[2] - \
+                     world_coord1[2]
+        cv2.putText(show_img, f"dx: {dx:.2f}mm, dy: {dy:.2f}mm, dz: {dz:.2f}mm,",
+                    (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 8)
+        # show point coordinates
+        x1, y1, z1 = world_coord1[0].astype(np.float32), world_coord1[1].astype(np.float32), world_coord1[1].astype(
+            np.float32)
+        x2, y2, z2 = world_coord2[0].astype(np.float32), world_coord2[1].astype(np.float32), world_coord2[1].astype(
+            np.float32)
+        cv2.putText(show_img, f"point1: ({x1:.1f}, {y1:.1f}, {z1:.1f})", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 5,
+                    (0, 255, 0), 8)
+        cv2.putText(show_img, f"point2: ({x2:.1f}, {y2:.1f}, {z2:.1f})", (20, 600), cv2.FONT_HERSHEY_SIMPLEX, 5,
+                    (0, 255, 0), 8)
+        cv2.putText(show_img, '1', (point1[0], point1[1] + 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 12)
+        cv2.putText(show_img, '2', (point2[0], point2[1] + 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 12)
+        # show end_points
+        point1, point2 = self.endpoints[-2:]
+        point1_left = Ruler.draw_line_crop(self.left_img, point1[0])
+        point1_right = Ruler.draw_line_crop(self.right_img, point1[1])
+        point2_left = Ruler.draw_line_crop(self.left_img, point2[0])
+        point2_right = Ruler.draw_line_crop(self.right_img, point2[1])
+        p1 = cv2.resize(np.vstack([point1_left, point1_right]), [250, 500])
+        p2 = cv2.resize(np.vstack([point2_left, point2_right]), [250, 500])
+        show_img = cv2.resize(show_img, [500, 500])
+        result = np.concatenate([p1, show_img, p2], axis=1)
+        imshow('Image Result', result)
 
 
 
@@ -154,7 +187,7 @@ class Ruler():
         p1 = np.hstack([point1_left, point1_right])
         p2 = np.hstack([point2_left, point2_right])
         endpoints = np.vstack([p1, p2])
-        endpoints = cv2.resize(endpoints, [600, 600])
+        # endpoints = cv2.resize(endpoints, [600, 600])
 
         imshow('First row: point 1;     Second row: point 2', endpoints)
 
@@ -164,10 +197,10 @@ class Ruler():
     def draw_line_crop(img, point):
         """Draw a corss around the corner"""
         d = 100
-        line_thickness = 1
+        line_thickness = 3
         point = (int(point[0]), int(point[1]))
-        cv2.line(img, point, (point[0], 0), (0,0,0), thickness=line_thickness)
-        cv2.line(img, point, (0, point[1]), (0,0,0), thickness=line_thickness)
+        cv2.line(img, point, (point[0], 0), (0,0,255), thickness=line_thickness)
+        cv2.line(img, point, (0, point[1]), (0,0,255), thickness=line_thickness)
         return \
             img[point[1]-d:point[1]+d,
                 point[0]-d:point[0]+d,]
