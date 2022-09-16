b2d213fbc186fc98f512ffbff98e8735e1233e06
handysome6 <handysome6@gmail.com>
2022-09-16 17:57:28 +0800

    updated GUI slots

--- qt/qt_main.py
+++ qt/qt_main.py
@@ -17,6 +17,9 @@ from numpy import empty
 from qt.qt_calibrate import CalibWindow
 from qt.qt_view_measure import ViewMeasureWindow
 from qt.main_ui import Ui_Form
+from calib.preprocess import Preprocess
+from calib.calibration import Calibrate
+from model.camera_model import CameraModel
 
 
 class MainWindow(QWidget):
@@ -41,23 +44,67 @@ class MainWindow(QWidget):
         if str_path != '':
             self.project_folder = Path(str_path).relative_to(Path('.').resolve())
             print("Selected project folder: " + str(self.project_folder))
-            self.project_folder_container.setText(str(self.project_folder))
-            self._load_camera_model_combo()
-            self._load_recent_photo_list()
+            self.ui.folderLineEdit.setText(str(self.project_folder))
 
-    
+    @pyqtSlot()
+    def on_startCalibrate_clicked(self):
+        print("startCalibrate!")
+        if self.project_folder is None:
+            print("project folder not selected")
+            return 
+
+        # start calibration
+        # camera info
+        image_size = (4056, 3040)       # Raspi  IMX477
+        image_size = (4032, 3040)       # Jetson IMX477
+        is_fisheye = False
+        # Hyperparams
+        operation_folder = self.project_folder
+        rows = 11
+        columns = 8
+        CHECKERBOARD = (rows,columns)
+        square_size = 60
+
+        camera = CameraModel(image_size, is_fisheye)
+        preprocess = Preprocess(camera, operation_folder,
+            CHECKERBOARD=CHECKERBOARD, square_size=square_size)
+        preprocess.preprocess_sbs()
+        print()
+
+        calibration = Calibrate(camera, operation_folder,
+            CHECKERBOARD=CHECKERBOARD, square_size=square_size)
+        calibration.calibrate_left_right()
+        calibration.stereo_calibrate(fix_intrinsic = False, show_pve = False)
+        print()
+
+        self.ui.modelLineEdit.setText(str(self.project_folder / "camera_model" / "camera_model.json"))
+        
+
+
     @pyqtSlot()
     def on_selectModel_clicked(self):
         print("selcect model!")
+        str_path, _ = QFileDialog.getOpenFileName(self, "Select camera model...", "datasets", "JSON *.json")
+        if str_path != '':
+            self.camera_path = Path(str_path).relative_to(Path('.').resolve())
+            print("Selected camera model: " + str(self.camera_path))
+            self.ui.modelLineEdit.setText(str(self.camera_path))
 
     @pyqtSlot()
     def on_selectImage_clicked(self):
         print("select image!")
+        str_path, _ = QFileDialog.getOpenFileName(self, "Select image...", "datasets", "*.png *.jpg")
+        if str_path != '':
+            self.image_path = Path(str_path).relative_to(Path('.').resolve())
+            print("Selected image: " + str(self.image_path))
+            self.ui.imageLineEdit.setText(str(self.image_path))
 
 
     def _open_view_measure_window(self):
         assert self.current_model.is_file()
         assert self.current_image.is_file()
+        # further implement  - TODO
+
         # self.view_measure_window = ViewMeasureWindow(
         #     self.current_model, self.current_image, self.project_folder,
         #     parent=self)
